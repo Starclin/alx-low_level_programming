768e53ef070512239d5bc26117f531a9f40e3455
Starclin <chikezie868@gmail.com>
2022-06-06 15:31:54 -0700

    print Alphbt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	const char *text = "abcdfghijklmnoprstuvwxyz\n";
+	int i;
+
+	for (i = 0, text[i] != 0, i++)
+	{
+		putchar(text[i]);
+	}
+	return (0);
+}
