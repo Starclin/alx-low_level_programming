084fdc9004de47119715f32d0681beb380fc88e8
Starclin <chikezie868@gmail.com>
2022-06-07 17:08:26 -0700

    is LOWER FUNC

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _islower - Entry Point
+ *
+ * Return:0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
