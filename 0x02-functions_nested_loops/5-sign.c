08b6eee7d4863757a19c558f704684f4cc9646e3
Starclin <chikezie868@gmail.com>
2022-06-07 18:26:21 -0700

    Signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_sign - Entry Point
+ * @n : number for sign convention
+ * Return: 0 or 1 or -1
+ */
+int print_sign(int n)
+{
+	char c = '+';
+	char d = '0';
+	char e = '-';
+
+	if (n > 0)
+	{
+		putchar(c);
+		return (1);
+	}
+	else if (n = 0)
+	{
+		putchar(d);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar(e);
+		return(-1);
+	}
+}
