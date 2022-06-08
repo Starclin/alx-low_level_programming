d846ad53bf08ff0dc332c332ae60dbcd696853a8
Starclin <chikezie868@gmail.com>
2022-06-07 18:32:27 -0700

    Fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,22 +8,4 @@
 int print_sign(int n)
 {
 	char c = '+';
-	char d = '0';
-	char e = '-';
-
-	if (n > 0)
-	{
-		putchar(c);
-		return (1);
-	}
-	else if (n = 0)
-	{
-		putchar(d);
-		return (0);
-	}
-	else if (n < 0)
-	{
-		putchar(e);
-		return(-1);
-	}
-}
+	char d = '
