9ab0f846a670a902ce8932e2fac768f8303a9311
Starclin <chikezie868@gmail.com>
2022-06-09 17:38:46 -0700

    Print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0-9
+ */
+void print_numbers(void)
+{
+	const char *text = "0123456789\n";
+	int i;
+
+	for (i = 0, text[i] != 0, i++)
+	{
+		putchar(text[i]);
+	}
+}
+
