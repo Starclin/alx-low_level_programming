0ae1a3b6fed3d4d13a1d01cdf73c6518ca2598aa
Starclin <chikezie868@gmail.com>
2022-06-09 18:58:58 -0700

    Diagonalier

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints a diagonal n number of times
+ * @n: number of times diagonal is to be printed
+ */
+void print_line(int n)
+{
+	while (n != 0 && n > 0)
+	{
+		const char *t = "\\";
+		int i;
+
+		for (i = 0; t[i] != 0; i++)
+		{
+			putchar(t[i]);
+		}
+		n--;
+	}
+	if (n != 0)
+	{
+		const char *ne = "\n";
+		int j;
+
+		for (j = 0; ne[j] != 0; j++)
+		{
+			putchar(ne[j]);
+		}
+	}
+	else if (n <= 0)
+	{
+		const char *news = "\n";
+		int k;
+
+		for (k = 0; news[k] != 0; k++)
+		{
+			putchar(news[k]);
+		}
+	}
+}
