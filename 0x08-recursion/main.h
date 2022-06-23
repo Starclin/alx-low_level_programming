831e142ba2fa62c1c1be9d7ab9a35869294da9e3
Starclin <101003544+Starclin@users.noreply.github.com>
2022-06-24 00:18:52 +0100

    main head

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,10 @@
-void _puts_rucursion(char *s);
-void _putchar(char c);
-void print_rev_recursion(char *s);
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int square(int n, int val);
+int _sqrt_recursion(int n);
+int check_prime(int n, int resp);
+int is_prime_number(int n);
