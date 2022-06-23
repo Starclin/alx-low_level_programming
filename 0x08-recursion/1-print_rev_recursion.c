a0e3bc97b8a4661260530b955fc6b3fd120a745e
Starclin <101003544+Starclin@users.noreply.github.com>
2022-06-24 00:11:55 +0100

    print rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _print_rev_recursion - prints reverse recursions in forward
- * @s:pointer to string
- * Return:void
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to the string
+ * Return: void
  */
+
 void _print_rev_recursion(char *s)
 {
-	if (*s == 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(s[-1]);
-		_print_rev_recursion(s - 1);
-	}
+if (*s > '\0')
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+
 }
